64713e4c0060192ba8e644e5c1b0fff347b55036
RillVicosuh <rickvill00@gmail.com>
2021-11-17 22:42:38 -0800

    Editing the factory.hpp file

--- factory.hpp
+++ factory.hpp
@@ -7,6 +7,30 @@
 
 class Factory{
   private:
+    Base* createExpression(Base* leftValue, Base* rightValue, char currOp){
+      if(currOp == '+'){
+        return new Add(leftValue, rightValue);
+      }
+      else if(currOp == '-'){
+        return new Sub(leftValue, rightValue);
+      }
+      else if(currOp == '*'){
+        return new Mult(leftValue, rightValue);
+      }
+      else if(currOp == '/'){
+        if(rightValue->evaluate() == 0){
+	  cout << "Denominator cannot be 0" << endl;
+          return nullptr;
+        }
+        return new Div(leftValue, rightValue);
+      }
+      else if(currOp == '^'){
+        return new Pow(leftValue, rightValue);
+      }
+      else{
+        return nullptr;
+      }
+    }
   
   public:
 
