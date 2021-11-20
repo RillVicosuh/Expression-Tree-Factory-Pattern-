09a3a5d990635dc13213981bb8347c4e23d637a6
RillVicosuh <rickvill00@gmail.com>
2021-11-20 10:42:33 -0800

    Removing code from factory.hpp and updating main.cpp

--- factory.hpp
+++ factory.hpp
@@ -7,30 +7,6 @@
 
 class Factory{
   private:
-    Base* createExpression(Base* leftValue, Base* rightValue, char currOp){
-      if(currOp == '+'){
-        return new Add(leftValue, rightValue);
-      }
-      else if(currOp == '-'){
-        return new Sub(leftValue, rightValue);
-      }
-      else if(currOp == '*'){
-        return new Mult(leftValue, rightValue);
-      }
-      else if(currOp == '/'){
-        if(rightValue->evaluate() == 0){
-	  cout << "Denominator cannot be 0" << endl;
-          return nullptr;
-        }
-        return new Div(leftValue, rightValue);
-      }
-      else if(currOp == '^'){
-        return new Pow(leftValue, rightValue);
-      }
-      else{
-        return nullptr;
-      }
-    }
   
   public:
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 
 int main(int argc, char**argv){
+  Factory* expTree = new Factory();
 
+  for(int i = 0; i < argc; i++){
+    cout << argv[i] << endl;
+  }
+
+  Base* output = expTree->parse(argv, argc);
+
+  if(output){
+    cout << output->stringify() << " = " << output->evaluate() << endl;
+  }
+  else{
+    cout << "Expression tree is incorrect" << endl;
+  }
   return 0;
 }
