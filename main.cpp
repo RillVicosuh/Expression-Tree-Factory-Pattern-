041e2cce4d340f7803a4b5eeacbe00011ebb0a5d
RillVicosuh <rickvill00@gmail.com>
2021-12-02 00:26:21 -0800

    Updating main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 #include "base.hpp"
 
 int main(int argc, char**argv){
+  Factory* expTree = new Factory();
 
+  for(int i = 0; i < argc; i++){
+    cout << argv[i] << endl;
+  }
+
+  Base* output = expTree->parse(argv, argc);
+
+  if(output){
+    cout << output->stringify() << " = " << output->evaluate() << endl;
+  }
+  else{
+    cout << "Expression tree is incorrect" << endl;
+  }
   return 0;
 }
